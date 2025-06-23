c5a3bf711ffa1045674ff2dc11bad240069bd26b
Martin Muthaura <mmuthaura 06@gmail.com>
2025-06-23 12:35:51 +0300

    Add divide, square, and add functions using arrow syntax

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. A simple arrow function that returns a value
+const divide = () => 2000 / 100;
+
+// 2. Arrow function with one parameter
+const square = x => x * x;
+
+// 3. Arrow function with two parameters
+const add = (a, b) => a + b;
